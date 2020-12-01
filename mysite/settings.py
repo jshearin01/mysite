dca2eecca0516d55f66e81bcc4f04be902acfa3f
John Shearin <jshearin01@gmail.com>
2020-12-01 10:20:10 -0500

    push to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'crp(1!x@_2!c-88(imlen(jmmurq2t-l_(4xviyy4ptl!@oc+l'
+# SECRET_KEY in .env
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
